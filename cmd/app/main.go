3606babc922c04a6c47660c4803cbfa0283aea39
Atishay Jain <jainatishay.j@gmail.com>
2018-11-21 17:09:06 +0530

    updates the main.go file

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"Go-Social/pkg"
+	"Go-Social/pkg/config"
+	"Go-Social/pkg/mongo"
+	"Go-Social/pkg/server"
+)
+
+type App struct {
+	server  *server.Server
+	session *mongo.Session
+	config  *root.Config
+}
+
+func (a *App) Initialize() {
+	a.config = config.GetConfig()
+	var err error
+	a.session, err = mongo.NewSession(a.config.Mongo)
+	if err != nil {
+		log.Fatalln("unable to connect to mongodb")
+	}
+
+	u := mongo.NewUserService(a.session.Copy(), a.config.Mongo)
+	a.server = server.NewServer(u, a.config)
+}
+
+func (a *App) Run() {
+	fmt.Println("Welcome back, Atishay")
+	defer a.session.Close()
+	a.server.Start()
+}
+
 func main() {
 	a := App{}
 	a.Initialize()
