7a679e357a8464211ff4d6ce1a1c192b43bb1468
Atishay Jain <jainatishay.j@gmail.com>
2018-11-24 17:11:37 +0530

    file name changed

--- pkg/mongo/user_service.go
+++ pkg/mongo/user_service.go
@@ -15,3 +15,11 @@ func NewUserService(session *mgo.Session, config *root.MongoConfig) *UserService
 	collection.EnsureIndex(userModelIndex())
 	return &UserService{collection}
 }
+
+func (p *UserService) CreateUser(u *root.User) error {
+	user, err := newUserModel(u)
+	if err != nil {
+		return err
+	}
+	return p.collection.Insert(&user)
+}
